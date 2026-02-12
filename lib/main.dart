9db6611d70e707ade381f566f9d77ac77257fe4a
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-02-12 19:24:44 +0530

    The Pubyml  has been updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tic_tac_toe/Pages/home_page.dart';
+
+
 
 void main() {
   runApp(const MyApp());
@@ -7,6 +10,20 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+static final TextStyle myNewFont = GoogleFonts.pressStart2p(
+  textStyle: const TextStyle(
+    color: Colors.black,
+    letterSpacing: 3,
+  ),
+);
+static final TextStyle myNewWhiteFont = GoogleFonts.pressStart2p(
+  textStyle: const TextStyle(
+    color: Colors.white,
+    letterSpacing: 3,
+    fontSize:15
+  ),
+);
 
   // This widget is the root of your application.
   @override
